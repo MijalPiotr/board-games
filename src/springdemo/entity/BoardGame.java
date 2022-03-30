f59977e1da5fccea6adba8a8bd3bab26f54bb3c8
Piotr Mijał <p.mijal@wp.pl>
2022-03-30 12:15:55 +0200

    Klasa BoardGame z adnotacjami hibernata

--- src/springdemo/entity/BoardGame.java
+++ src/springdemo/entity/BoardGame.java
@@ -24,5 +24,47 @@ public class BoardGame {
 	
 	@Column(name="publisher")
 	private String publisher; 
+	
+	public BoardGame () {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardGame [id=" + id + ", title=" + title + ", author=" + author + ", publisher=" + publisher + "]";
+	}
+	
 
 }
